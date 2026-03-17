fd39a13c0bfebc19e4e980ee0c8b9d59ec4c2492
BBOXROCKS <137446942+BBOXROCKS@users.noreply.github.com>
2026-03-17 10:06:40 -0700

    Implement DoubleCalculator with basic arithmetic methods

--- Lab8/src/main/java/edu/sdsu/cs160l/calculator/DoubleCalculator.java
+++ Lab8/src/main/java/edu/sdsu/cs160l/calculator/DoubleCalculator.java
@@ -1,1 +1,18 @@
+public class DoubleCalculator implements Calculator<Double> {
+    public Double add(Double a, Double b) {
+        return a + b;
+    }
 
+    public Double sub(Double a, Double b) {
+        return a - b;
+    }
+
+    public Double div(Double a, Double b) {
+        if (b == 0) return Double.POSITIVE_INFINITY;
+        return a / b;
+    }
+
+    public Double mul(Double a, Double b) {
+        return a * b;
+    }
+}
