17e3f42240f6edbfdd1778f5dc6c6db34880ab56
BBOXROCKS <137446942+BBOXROCKS@users.noreply.github.com>
2026-03-17 10:07:45 -0700

    Implement IntegerCalculator with basic arithmetic operations

--- Lab8/src/main/java/edu/sdsu/cs160l/calculator/IntegerCalculator.java
+++ Lab8/src/main/java/edu/sdsu/cs160l/calculator/IntegerCalculator.java
@@ -1,4 +1,20 @@
 package edu.sdsu.cs160l.calculator;
 
-public class IntegerCalculator {
+public class IntegerCalculator implements Calculator<Integer> {
+    public Integer add(Integer a, Integer b) {
+        return a + b;
+    }
+
+    public Integer sub(Integer a, Integer b) {
+        return a - b;
+    }
+
+    public Integer div(Integer a, Integer b) {
+        if (b == 0) throw new ArithmeticException();
+        return a / b;
+    }
+
+    public Integer mul(Integer a, Integer b) {
+        return a * b;
+    }
 }
